2acfd3f8122d369de0477276e8810e73325e2fa9
Estygrogan <dbobbet@gmail.com>
2023-02-12 22:06:12 +0000

    Add board class code to set up eventual board layout

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
-class board:
+class Board:
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
     
+    def print(self):
+        
