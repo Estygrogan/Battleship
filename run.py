c2d7dd19f518870d37026313d143b32408411977
Estygrogan <dbobbet@gmail.com>
2023-02-16 00:37:18 +0000

    Change from board_type to type in board class

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ scores = {"player": 0, "computer": 0}
 
 class Board:
 
-    def __init__(self, size, num_ships, name, board_type):
+    def __init__(self, size, num_ships, name, type):
         self.size = size
         self.board = [["." for x in range(size)] for y in range(size)]
         self.num_ships = num_ships
